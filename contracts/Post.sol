6a95f57e22125b7031046365f633eb3c6ebee46d
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-12-19 14:47:34 +0900

    constructor for the post

--- contracts/Post.sol
+++ contracts/Post.sol
@@ -2,5 +2,26 @@
 pragma solidity ^0.8.16;
 
 contract Post {
-    constructor() public {}
+    address public author;
+    string public postTitle;
+    uint256 public likeCount;
+    string public tag;
+    string public timestamp;
+    string public content;
+
+    constructor(
+        string memory _title,
+        address _author,
+        string memory _tag,
+        string memory _timestamp,
+        string memory _content
+    ) public {
+        postTitle = _title;
+        author = _author;
+        tag = _tag;
+        timestamp = _timestamp;
+        content = _content;
+
+        likeCount = 0;
+    }
 }
